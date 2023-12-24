65d6f9250aaae3b7c825a9858eb8354a897af31e
Moises-TR <moisestrigueros@hotmail.com>
2023-12-24 08:43:28 -0600

    chore: install test dependencies

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testTimeout: 15000,
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["js", "json", "ts"],
+  roots: ["src", "tests"],
+  moduleDirectories: ["node_modules"],
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^src(.*)$": "<rootDir>/src/$1",
+  },
+  testRegex: "((\\.|/*.)(spec))\\.ts?$",
+};
