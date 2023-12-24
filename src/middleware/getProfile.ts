313a42ae05ed3b8928b196999baf6ff1ec235f10
Moises-TR <moisestrigueros@hotmail.com>
2023-12-23 21:22:43 -0600

    refactor: update profile middleware to use Profile from models

--- src/middleware/getProfile.ts
+++ src/middleware/getProfile.ts
@@ -1,14 +1,13 @@
 import { NextFunction, Response } from "express";
+import { Profile } from "../model"
 
 export const getProfile = async (
   req: any,
   res: Response,
   next: NextFunction
 ) => {
-  const { Profile } = req.app.get("models");
-  const profile = await Profile.findOne({
-    where: { id: req.get("profile_id") || 0 },
-  });
+  const id = req.get("profile_id") ?? 0;
+  const profile = await Profile.findOne({ where: { id } })
   if (!profile) return res.status(401).end();
   req.profile = profile;
   return next();
